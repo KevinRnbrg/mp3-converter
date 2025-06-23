67be3df6c41468a0e4d50b5dd13b0a98057637cb
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2025-06-23 17:56:37 +0300

    rewrite logic to fix issues with connectivity

--- main.py
+++ main.py
@@ -1,37 +1,28 @@
-# Don't save mp4 file to pc, only mp3
-# create new folder to desktop with mp3 files inside
-# can add many links, will show converting progress
-
 import os
-from moviepy.editor import *
+from pytubefix import YouTube
+from moviepy import *
 
-def download_MP4_from_YT(link):
-    print("Downloading MP4 from YouTube")
-    mp4_file = print("logic to find mp4 file from yt")
-    return mp4_file
+dir_name = "audio"
 
-def convert_MP4_to_MP3(file_name): # surround with try?
-    print("Converting mp4 to mp3")
-    video = VideoFileClip(f"{file_name}.mp4")
-    video.audio.write_audiofile(f"{file_name}.mp3")
+def getVideoFileFromURL(yt_url):
+    yt = YouTube(yt_url)
+    stream = yt.streams.filter(only_audio=True).first()
+    return stream.download(filename='temp_audio.mp4')
+
+def writeAudioFileFromVideo(download_path):
+    output_mp3_path = os.path.join(dir_name, "output.mp3")
+    audio = AudioFileClip(download_path)
+    audio.write_audiofile(output_mp3_path)
+    audio.close()
 
 def main():
-    print("Start application")
-    yt_link = input("Paste YouTube link:")
-    mp4_file = download_MP4_from_YT(yt_link)
-    # convert_MP4_to_MP3(mp4_file)
-    dir_name = "mp3_files"
-    try:
+    yt_url = input("Paste YouTube link: ")
+    video_file = getVideoFileFromURL(yt_url)
+    if not os.path.exists(dir_name):
         os.mkdir(dir_name)
-        print(f'Directory {dir_name} created successfully.')
-        # add mp3 files inside
-    except FileExistsError:
-        print(f"Directory '{dir_name}' already exists.") # add fiels inside
-    except PermissionError:
-        print(f"Permission denied: Unable to create '{dir_name}'.")
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        
-    print("Converting done") # + add filepath to show where files are
+    writeAudioFileFromVideo(video_file)
+    if os.path.exists(video_file):
+        os.remove(video_file)
+    print("Audio file saved.")
 
 main()
