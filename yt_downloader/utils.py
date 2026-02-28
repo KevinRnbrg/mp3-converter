b773b9c332021ac079de3afe4309cf4bde904feb
Kevin Rosenberg <kevinrosenberg04@gmail.com>
2026-02-28 23:18:08 +0200

    refactor the codebase to better follow DRY and SOLID; add test

--- yt_downloader/utils.py
+++ yt_downloader/utils.py
@@ -4,14 +4,14 @@ import os
 
 # Write tests
 
-def remove_video_file(video_file): # Mock os.path.exists, os.remove
+def remove_video_file(video_file: str | None) -> None:
     try:
-        if os.path.exists(video_file) and video_file is not None:
+        if video_file is not None and os.path.exists(video_file):
             os.remove(video_file)
-    except (FileNotFoundError, PermissionError, OSError) as e:
-        raise e
+    except (FileNotFoundError, PermissionError, OSError):
+        raise
 
-def get_formatted_title(title: str):
+def get_formatted_title(title: str) -> str:
     slugified = slugify(
         title, 
         max_length=config.TITLE_MAX_LENGTH,
